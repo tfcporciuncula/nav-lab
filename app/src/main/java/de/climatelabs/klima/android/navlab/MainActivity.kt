900bcdd20d4ae169d7539a146528ca4f38b0dddd
Fred Porciúncula <tfcporciuncula@gmail.com>
2021-11-23 22:39:04 +0100

    Empty project with up-to-date bare bones and Hilt

---
+++ app/src/main/java/de/climatelabs/klima/android/navlab/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.climatelabs.klima.android.navlab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
+  }
+}
---
+++ app/src/main/java/de/climatelabs/klima/android/navlab/NavLabApplication.kt
@@ -0,0 +1,7 @@
+package de.climatelabs.klima.android.navlab
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NavLabApplication : Application()
